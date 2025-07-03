21ae23afb58513add6cacc67b3c5e6c601efc418
AugustoSus26 <145451648+AugustoSus26@users.noreply.github.com>
2025-07-03 08:36:36 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,42 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
-numeroSenha.textContent = tamanho;
+numeroSenha.textContent = tamanhoSenha;
 
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 
 botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
 
 function diminuiTamanho(){
-    tamanhoSenha = tamanhoSenha-1;
-    numeroSenha.textContent = tamanho;
+    if(tamanhoSenha > 1){
+        tamanhoSenha = tamanhoSenha-1;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
 
-console.log(botoes);
+function aumentaTamanho(){
+    if(tamanhoSenha < 26){
+        tamanhoSenha = tamanhoSenha+1;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+
+console.log(checkbox);
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+geraSenha();
+
+function geraSenha(){
+    let senha = ''
+    for(let i = 0; i < tamanhoSenha;i++){
+    let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+    numeroAleatorio = Math.floor(numeroAleatorio);
+    senha = senha + letrasMaiusculas[numeroAleatorio];
+    }
+    campoSenha.value = senha;
+}
